3e33db17d8d7318e5e176d17e8737194a946cf4a
Abiodun Biobaku <biolafrica@gmail.com>
2025-06-18 01:58:37 +0100

    create an extended product repository

--- packages/utils/database/baseRepository.jsx
+++ packages/utils/database/baseRepository.jsx
@@ -79,5 +79,4 @@ export class BaseRepository{
     return data
   }
 
-
 }
---
+++ packages/utils/database/productRepository.jsx
@@ -0,0 +1,60 @@
+import { supabase } from "./supabaseClient";
+import { BaseRepository } from "./baseRepository";
+
+export class ProductRepository extends BaseRepository{
+
+  constructor (restaurantId){
+    super("products", restaurantId)
+  }
+
+  async findAllWithFK({searchTerm="", categoryId = null, branchId=null, range=[0,9]}={}){
+    let query = supabase
+    .from(this.table)
+    .select(`
+      id, 
+      name, 
+      image_url, 
+      price, 
+      description, 
+      category: Product_categories(name), 
+      branch: Branches(name)`,
+      {count: "exact"}
+    )
+
+    query = query.eq('restaurant_id', this.restaurantId)
+
+    if(searchTerm){
+      query = query.ilike("name", `%${searchTerm}`)
+    }
+
+    if(categoryId){
+      query = query.eq("category_id", categoryId)
+    }
+
+    if(branchId){
+      query = query.eq("branch_id", branchId)
+    }
+
+    query = query.range(range[0], range[1])
+
+    const {data, error, count} = await query
+    if(error) throw new Error (`[products] findAllWithFK failed: ${error.message}`)
+    return{data, count}
+  }
+
+  async findWithFKById(id){
+    const {data, error} = await supabase
+    .from(this.table)
+    .select(`
+      *, 
+      category:Product_categories(name), 
+      branch:Branches(name)
+    `)
+    .eq("id", id)
+    .maybeSingle()
+
+    if(error) throw new Error(`[products] findWithFKById failed: ${error.message}`)
+    return data
+  }
+  
+}
