01563c901cb3aef6f1801607dd874b3d7d13c6a6
Abiodun Biobaku <biolafrica@gmail.com>
2025-07-08 08:51:58 +0100

    created businessHourTable component for setting/hours page

--- apps/admin-dashboard/app/(Dashboard)/settings/general/hours/page.jsx
+++ apps/admin-dashboard/app/(Dashboard)/settings/general/hours/page.jsx
@@ -1,6 +1,49 @@
+"use client"
+
+import SettingsNav from "../../../../components/layout/settingsNav"
+import SettingsHeadingIntro from "../../../../components/pages/settingsHeadingIntro"
+import BackButton from "../../../../components/common/backButton"
+import { initial } from "../../../../data/formFields"
+import WeeklySchedule from "../../../../components/pages/businessHourTable"
+
 export default function Hours(){
+  function handleSave(updatedSchedules) {
+    console.log('Saving schedules:', updatedSchedules)
+    // send to backend
+  }
   return(
     <div className="hours_cont p-5 pt-30 lg:pl-75">
+
+      <SettingsHeadingIntro/>
+
+      <div className="border border-border-text rounded-md p-5 bg-white">
+
+        <div className="flex gap-5">
+
+          <div className="min-w-[270px] w-1/4 hidden lg:block">
+            <SettingsNav/>
+          </div>
+
+          <div className=" w-full lg:w-3/4 border p-5 rounded-md border-border-text">
+
+            <div className="border-b border-border-text flex items-center gap-2 mb-5 pb-2 font-semibold">
+              <BackButton/>
+              <h4>Business Hours</h4>
+            </div>
+
+            <WeeklySchedule
+              initialSchedules={initial}
+              onSave={handleSave}
+            />
+
+          </div>
+
+        </div>
+
+        
+       
+
+      </div>
 
     </div>
   )
--- apps/admin-dashboard/app/data/formFields.jsx
+++ apps/admin-dashboard/app/data/formFields.jsx
@@ -9,3 +9,14 @@ export const storeFields = [
   { name: 'tiktokLink', label: 'Tiktok Link', type: 'text', required: false },
   { name: 'prefix', label: 'Prefix(Two letters before your OrderID)', type: 'text', required: true },
 ]
+
+export const initial = [
+  { day: 'Monday',    enabled: true,  from: '09:00', to: '17:00' },
+  { day: 'Tuesday',   enabled: true,  from: '09:00', to: '17:00' },
+  { day: 'Wednesday',   enabled: true,  from: '09:00', to: '17:00' },
+  { day: 'Thursday',   enabled: true,  from: '09:00', to: '17:00' },
+  { day: 'Friday',   enabled: true,  from: '09:00', to: '17:00' },
+  { day: 'Satday',   enabled: true,  from: '09:00', to: '17:00' },
+  { day: 'Sunday',   enabled: false,  from: '09:00', to: '17:00' },
+ 
+]
