c85b64a8845d57836c0880c9448266f21e5dea32
Abiodun Biobaku <biolafrica@gmail.com>
2025-07-17 00:17:31 +0100

    component file clean up

--- apps/admin-dashboard/app/components/pages/settingsHeadingIntro.jsx
+++
@@ -1,8 +0,0 @@
-export default function SettingsHeadingIntro(){
-  return(
-    <>
-      <h2 className="text-3xl font-semibold pl-5">Settings</h2>
-      <h4 className="text-md text-sec-text font-light mb-10 pl-5">A centralized place to manage the brand	&#39;s details, opening hours, and preferences</h4>
-    </>
-  )
-}
